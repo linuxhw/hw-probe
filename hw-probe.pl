f89f2384347f8b7389907a50e4bf2958949666a1
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2018-10-30 11:33:54 +0100

    split's first arg is a regex and $_ is the default arg

--- hw-probe.pl
+++ hw-probe.pl
@@ -8337,8 +8337,8 @@ sub showInfo()
     
     foreach (split(/\s*\n\s*/, readFile($ShowDir."/devices")))
     {
-        my @Info = split(";", $_);
-        
+        my @Info = split(/;/);
+
         my %Dev = (
             "ID"      => $Info[0],
             "Class"   => $Info[1],
