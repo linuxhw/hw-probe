716868dea277f8930c3cedf1db43c11e5d861cc6
Alex Samorukov <samm@os2.kiev.ua>
2021-03-18 22:05:44 +0100

    rewrite change according to PR review

--- hw-probe.pl
+++ hw-probe.pl
@@ -3753,16 +3753,10 @@ sub probeHW()
         }
     }
     
-    if(not $Sys{"Type"} and not $Sys{"Model"})
+    if($Kldstat_v=~/bcm2835_/)
     {
-        for my $str ($Kldstat_v) {
-          my $RPIMatch = index($str, "bcm2835_")!=-1;
-          if($RPIMatch)
-          {
-            $Sys{"Type"} = "system on chip";
-            $Sys{"Model"} = "Raspberry Pi";
-          }
-       }
+        $Sys{"Type"} = "system on chip";
+        $Sys{"Model"} = "Raspberry Pi";
     }
     
     my $Modstat = "";
